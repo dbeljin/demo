2225c7c1d3b54f862dcad3f32d5436e3c7753d24
Dusan Beljin <dbeljin@gmail.com>
2025-03-20 09:04:09 +0100

    Increased timeout interval to 30sec

--- DemoApp/Utils/Constants.swift
+++ DemoApp/Utils/Constants.swift
@@ -9,6 +9,6 @@ import Foundation
 
 struct Constants {
     static let apiUrl = "https://run.mocky.io/v3/d403fba7-413f-40d8-bec2-afe6ef4e201e"
-    static let timeoutInterval: TimeInterval = 10
+    static let timeoutInterval: TimeInterval = 30
     static let itemResponseFileName = "item_response"
 }
